4b771c8da014e644ae31bc05e89d312a2065bb63
xiyaozhuang <xiyao.zhuang@nhs.net>
2022-11-14 16:34:15 +0000

    update test suite

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,33 +3,25 @@ const app = require("../app");
 const connection = require("../db/connection");
 const seed = require("../db/seeds/seed");
 const data = require("../db/data/test-data");
+const { string } = require("pg-format");
 
 beforeEach(() => seed(data));
 afterAll(() => connection.end());
 
-describe("/api/categoriess", () => {
+describe("/api/categories", () => {
   test("GET:200 sends an array of categories to the client", () => {
-    const expectedOutput = [
-      {
-        slug: "euro game",
-        description: "Abstact games that involve little luck",
-      },
-      {
-        slug: "social deduction",
-        description: "Players attempt to uncover each other's hidden role",
-      },
-      { slug: "dexterity", description: "Games involving physical skill" },
-      {
-        slug: "children's games",
-        description: "Games suitable for children",
-      },
-    ];
-
     return request(app)
       .get("/api/categories")
       .expect(200)
       .then((res) => {
-        expect(res.body.categories).toEqual(expectedOutput);
+        expect(res.body.categories).toEqual(
+          expect.arrayContaining([
+            expect.objectContaining({
+              slug: expect.any(String),
+              description: expect.any(String),
+            }),
+          ])
+        );
       });
   });
 });
